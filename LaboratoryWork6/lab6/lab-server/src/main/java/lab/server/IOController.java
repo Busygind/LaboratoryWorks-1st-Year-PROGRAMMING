a308f3c5e5fc3e8199dc3d001a216ee2e31d290e
Dmirtiy Busygin <Busygin.voll@mail.ru>
2022-03-24 19:23:06 +0300

    refactor server in NIO structure

---
+++ LaboratoryWork6/lab6/lab-server/src/main/java/lab/server/IOController.java
@@ -0,0 +1,29 @@
+package lab.server;
+
+import lab.common.util.commands.CommandAbstract;
+import lab.common.util.entities.CollectionManager;
+import lab.common.util.exceptions.DisconnectInitException;
+import lab.common.util.handlers.TextFormatter;
+import lab.common.util.requestSystem.Response;
+import lab.common.util.requestSystem.Serializer;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
+
+public class IOController {
+
+    public static CommandAbstract getCommand(SocketChannel in) throws IOException, ClassNotFoundException {
+        ByteBuffer readBuffer = ByteBuffer.allocate(4096);
+        in.read(readBuffer);
+        return Serializer.deserializeCommand(readBuffer.array());
+    }
+
+    public static Response buildResponse(CommandAbstract command, CollectionManager manager) throws DisconnectInitException {
+        if (command.getName().equals("exit")) {
+            TextFormatter.printInfoMessage("Client initialize disconnect");
+            throw new DisconnectInitException("Client initialize disconnect");
+        }
+        return new Response(command.execute(manager), true);
+    }
+}
--- LaboratoryWork6/lab6/lab-server/src/main/java/lab/server/Server.java
+++ LaboratoryWork6/lab6/lab-server/src/main/java/lab/server/Server.java
@@ -3,24 +3,30 @@ package lab.server;
 import lab.common.util.commands.CommandAbstract;
 import lab.common.util.entities.CollectionManager;
 import lab.common.util.entities.Dragon;
+import lab.common.util.exceptions.DisconnectInitException;
 import lab.common.util.handlers.HistorySaver;
 import lab.common.util.handlers.TextFormatter;
 import lab.common.util.requestSystem.Response;
+import lab.common.util.requestSystem.Serializer;
 import lab.server.fileHandlers.XMLReader;
-import lab.server.fileHandlers.XMLWriter;
 
 import java.io.File;
 import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.net.ServerSocket;
-import java.net.Socket;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.Iterator;
+import java.util.Set;
 
 
 public final class Server {
 
     private static final CollectionManager manager = new CollectionManager();
     private static final int SERVER_PORT = 45846;
+    private static Selector selector;
 
 //    Работа с файлом, хранящим коллекцию.
 //    Управление коллекцией объектов.
@@ -40,7 +46,82 @@ public final class Server {
 //        File file = new File(starting, fileName); // Initialize file from cmd
 //        Logger logger = LogManager.getLogger();
         File file = new File("C:\\Users\\Дмитрий\\JavaProjects\\LaboratoryWorks-1st-Year-PROGRAMMING\\LaboratoryWork6\\lab6\\Dragons.xml");
-        XMLReader reader = new XMLReader(); // Initialize parser
+        fillCollectionFromFile(file);
+        try {
+            selector = Selector.open();
+
+            ServerSocketChannel server = initChannel(selector);
+            startSelectorLoop(server);
+        } catch (IOException e) {
+            TextFormatter.printErrorMessage("Some problems with IO. Try again");
+        } catch (ClassNotFoundException e) {
+            TextFormatter.printErrorMessage("Trying to serialize non-serializable object");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private static void startSelectorLoop(ServerSocketChannel channel) throws IOException, ClassNotFoundException, InterruptedException {
+        while (true) {
+            int readyChannels = selector.select();
+            if (readyChannels == 0) {
+                continue;
+            }
+            startIteratorLoop(channel);
+        }
+    }
+
+    private static void startIteratorLoop(ServerSocketChannel channel) throws IOException, ClassNotFoundException, InterruptedException {
+        Set<SelectionKey> readyKeys = selector.selectedKeys();
+        Iterator<SelectionKey> iterator = readyKeys.iterator();
+        while (iterator.hasNext()) {
+            SelectionKey key = iterator.next();
+            iterator.remove();
+
+            if (key.isAcceptable()) {
+                SocketChannel socketChannel = channel.accept();
+
+                socketChannel.configureBlocking(false);
+                socketChannel.register(selector, SelectionKey.OP_READ);
+            } else if (key.isReadable()) {
+                SocketChannel socketChannel = (SocketChannel) key.channel();
+                CommandAbstract command = IOController.getCommand(socketChannel);
+                HistorySaver.addCommandInHistory(command);
+//                if (num > 0) {
+                    // Обработка входящих данных ...
+
+                    try {
+                        Response response = IOController.buildResponse(command, manager);
+                        ByteBuffer buffer = Serializer.serializeResponse(response);
+                        socketChannel.write(buffer);
+                        TextFormatter.printInfoMessage("Server wrote response to client");
+                    } catch (DisconnectInitException e) {
+                        TextFormatter.printInfoMessage(e.getMessage());
+                        socketChannel.close();
+                        break;
+                    }
+
+//                } else if (num == -1) {
+                    // -1 означает, что соединение было закрыто
+//                    socketChannel.close();
+//                    TextFormatter.printMessage("Channel closed");
+//                }
+            }
+        }
+    }
+
+    private static ServerSocketChannel initChannel(Selector selector) throws IOException {
+        ServerSocketChannel server = ServerSocketChannel.open();
+        TextFormatter.printInfoMessage("Socket opened");
+        server.socket().bind(new InetSocketAddress(SERVER_PORT));
+        server.configureBlocking(false);
+        server.register(selector, SelectionKey.OP_ACCEPT);
+        return server;
+    }
+
+    private static void fillCollectionFromFile(File file) {
+        XMLReader reader = new XMLReader();
         try {
             for (Dragon dragon : reader.read(file)) {
                 manager.addDragon(dragon);
@@ -56,64 +137,5 @@ public final class Server {
             TextFormatter.printErrorMessage("Данные в файле представлены в неверном формате, ошибка на уровне работы с данными");
             System.exit(0);
         }
-
-        try (ServerSocket server= new ServerSocket(SERVER_PORT)) {
-            // становимся в ожидание подключения к сокету под именем - "client" на серверной стороне
-            Socket client = server.accept();
-            // после хэндшейкинга сервер ассоциирует подключающегося клиента с этим сокетом-соединением
-            System.out.println("Connection accepted");
-            // инициируем каналы для общения в сокете, для сервера
-
-            // канал записи в сокет
-            ObjectOutputStream out = new ObjectOutputStream(client.getOutputStream());
-            System.out.println("ObjectOutputStream created");
-
-            // канал чтения из сокета
-            ObjectInputStream in = new ObjectInputStream(client.getInputStream());
-            System.out.println("ObjectInputStream created");
-
-            // начинаем диалог с подключенным клиентом в цикле, пока сокет не закрыт
-            while(!client.isClosed()) {
-
-                System.out.println("Server reading from channel");
-
-                // сервер ждёт в канале чтения получения данных клиента
-                CommandAbstract commandFromClient = (CommandAbstract) in.readObject();
-                HistorySaver.addCommandInHistory(commandFromClient);
-//                CommandAbstract command = (CommandAbstract) in.readObject();
-//                command.execute()
-                // после получения данных считывает их
-                System.out.println("READ from client message - " + commandFromClient);
-
-
-                // и выводит в консоль
-                System.out.println("Server try writing to channel");
-
-                if (commandFromClient.getName().equals("exit")) {
-                    TextFormatter.printInfoMessage("Client initialize disconnect");
-                    break;
-                }
-                // если условие окончания работы не верно - продолжаем работу -
-                // отправляем эхо-ответ обратно клиенту
-                out.writeObject(new Response(commandFromClient.execute(manager), true));
-                out.flush();
-                System.out.println("Server Wrote message to client.");
-            }
-
-            XMLWriter.write(file, manager);
-            TextFormatter.printMessage("Collection successfully saved");
-
-            System.out.println("Closing connections & channels.");
-            in.close();
-            out.close();
-
-            // потом закрываем сам сокет общения на стороне сервера!
-            client.close();
-            System.out.println("Closing connections & channels - DONE.");
-        } catch (IOException | ClassNotFoundException e) {
-            //todo обработать
-            e.printStackTrace();
-        }
-
     }
 }
