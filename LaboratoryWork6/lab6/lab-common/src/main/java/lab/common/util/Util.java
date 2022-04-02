3b057e6fbdc351887b6b2bd6c6b601ece6aaf651
Dmirtiy Busygin <Busygin.voll@mail.ru>
2022-04-03 02:08:23 +0300

    delete Util

--- LaboratoryWork6/lab6/lab-common/src/main/java/lab/common/util/Util.java
+++
@@ -1,7 +0,0 @@
-package lab.common.util;
-
-public final class Util {
-    private Util() {
-        throw new UnsupportedOperationException("This is an utility class and can not be instantiated");
-    }
-}
