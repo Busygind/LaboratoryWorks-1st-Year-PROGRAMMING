ca96c4fea20bfc64238c18294a748bcd48d62b00
Dmirtiy Busygin <Busygin.voll@mail.ru>
2022-04-03 15:41:28 +0300

    realize logger on server

--- LaboratoryWork6/lab6/lab-server/src/main/java/lab/server/Server.java
+++ LaboratoryWork6/lab6/lab-server/src/main/java/lab/server/Server.java
@@ -1,7 +1,6 @@
 package lab.server;
 
 import lab.common.util.commands.CommandAbstract;
-import lab.common.util.entities.CollectionManager;
 import lab.common.util.entities.Dragon;
 import lab.common.util.handlers.HistorySaver;
 import lab.common.util.handlers.TextFormatter;
@@ -25,49 +24,38 @@ import java.util.Set;
 
 public final class Server {
 
-    private static final CollectionManager manager = new CollectionManager();
-    private static final File starting = new File(System.getProperty("user.dir")); // Get current user directory
-    private static final int SERVER_PORT = 45846;
     private static Selector selector;
     private static File file;
-//    Работа с файлом, хранящим коллекцию.
-//    Управление коллекцией объектов.
+
 //    Назначение автоматически генерируемых полей объектов в коллекции.
-//    Ожидание подключений и запросов от клиента.
-//    Обработка полученных запросов (команд).
-//    Сохранение коллекции в файл при завершении работы приложения.
 //    Сохранение коллекции в файл при исполнении специальной команды, доступной только серверу (клиент такую команду отправить не может).
     private Server() {
         throw new UnsupportedOperationException("This is an utility class and can not be instantiated");
     }
 
     public static void main(String[] args) {
+        ServerConfig.logger.info("Server started");
         //todo засунуть все в application
 //        String fileName = args[0];
-//        file = new File(starting, fileName); // Initialize file from cmd
+//        file = new File(ServerConfig.starting, fileName); // Initialize file from cmd
         file = new File("C:\\Users\\Дмитрий\\JavaProjects\\LaboratoryWorks-1st-Year-PROGRAMMING\\LaboratoryWork6\\lab6\\Dragons.xml");
         fillCollectionFromFile(file);
         try {
             selector = Selector.open();
-
             ServerSocketChannel server = initChannel(selector);
             startSelectorLoop(server);
         } catch (IOException e) {
-            TextFormatter.printErrorMessage("Some problems with IO. Try again");
+            ServerConfig.logger.fatal("Some problems with IO. Try again");
         } catch (ClassNotFoundException e) {
-            TextFormatter.printErrorMessage("Trying to serialize non-serializable object");
+            ServerConfig.logger.error("Trying to serialize non-serializable object");
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-
     }
 
     private static void startSelectorLoop(ServerSocketChannel channel) throws IOException, ClassNotFoundException, InterruptedException {
         while (true) {
-            int readyChannels = selector.select();
-            if (readyChannels == 0) {
-                continue;
-            }
+            selector.select();
             startIteratorLoop(channel);
         }
     }
@@ -81,22 +69,22 @@ public final class Server {
 
             if (key.isAcceptable()) {
                 SocketChannel socketChannel = channel.accept();
-
+                ServerConfig.logger.info("Server get connection from " + socketChannel.getLocalAddress());
                 socketChannel.configureBlocking(false);
                 socketChannel.register(selector, SelectionKey.OP_READ);
             } else if (key.isReadable()) {
                 SocketChannel socketChannel = (SocketChannel) key.channel();
+                ServerConfig.logger.info("Client " + socketChannel.getLocalAddress() + " trying to send message");
                 CommandAbstract command = IOController.getCommand(socketChannel);
                 HistorySaver.addCommandInHistory(command);
                 try {
-                    Response response = IOController.buildResponse(command, manager);
+                    Response response = IOController.buildResponse(command, ServerConfig.manager);
                     ByteBuffer buffer = Serializer.serializeResponse(response);
                     socketChannel.write(buffer);
-                    TextFormatter.printInfoMessage("Server wrote response to client");
+                    ServerConfig.logger.info("Server wrote response to client");
                 } catch (DisconnectInitException e) {
-                    XMLWriter.write(file, manager);
-                    TextFormatter.printInfoMessage("Collection successfully saved");
-                    TextFormatter.printInfoMessage(e.getMessage());
+                    XMLWriter.write(file, ServerConfig.manager);
+                    ServerConfig.logger.info("Client init disconnect. Collection successfully saved");
                     socketChannel.close();
                     break;
                 }
@@ -106,8 +94,8 @@ public final class Server {
 
     private static ServerSocketChannel initChannel(Selector selector) throws IOException {
         ServerSocketChannel server = ServerSocketChannel.open();
-        TextFormatter.printInfoMessage("Socket opened");
-        server.socket().bind(new InetSocketAddress(SERVER_PORT));
+        ServerConfig.logger.info("Socket opened");
+        server.socket().bind(new InetSocketAddress(ServerConfig.SERVER_PORT));
         server.configureBlocking(false);
         server.register(selector, SelectionKey.OP_ACCEPT);
         return server;
@@ -117,17 +105,18 @@ public final class Server {
         XMLReader reader = new XMLReader();
         try {
             for (Dragon dragon : reader.read(file)) {
-                manager.addDragon(dragon);
+                ServerConfig.manager.addDragon(dragon);
                 if (dragon.getCreationDate() == null) {
                     dragon.setCreationDate();
                 }
             }
+            ServerConfig.logger.info("Collection successfully filled");
         } catch (IOException e) {
-            TextFormatter.printErrorMessage("Указанный файл не найден, попробуйте снова");
+            ServerConfig.logger.fatal("File doesn't exist");
             System.exit(0);
         } catch (Exception e) {
             //todo не понятно, какую ошибку бросает при неверном формате ввода
-            TextFormatter.printErrorMessage("Данные в файле представлены в неверном формате, ошибка на уровне работы с данными");
+            ServerConfig.logger.fatal("Can't parse file, data is incorrect");
             System.exit(0);
         }
     }
