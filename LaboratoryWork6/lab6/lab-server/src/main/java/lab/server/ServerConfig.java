152179dfa37d4a9beaae287eda1c5c0b65a54869
Dmirtiy Busygin <Busygin.voll@mail.ru>
2022-04-03 15:42:20 +0300

    remove static objects from main class to configuration

---
+++ LaboratoryWork6/lab6/lab-server/src/main/java/lab/server/ServerConfig.java
@@ -0,0 +1,14 @@
+package lab.server;
+
+import lab.common.util.entities.CollectionManager;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.File;
+
+public class ServerConfig {
+    protected static final CollectionManager manager = new CollectionManager();
+    protected static final Logger logger = LogManager.getLogger();
+    protected static final File starting = new File(System.getProperty("user.dir")); // Get current user directory
+    protected static final int SERVER_PORT = 45846;
+}
