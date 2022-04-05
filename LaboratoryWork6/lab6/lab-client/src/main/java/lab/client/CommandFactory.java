fcba55d49ab75ec1304489eeca78484612a0affd
Dmirtiy Busygin <Busygin.voll@mail.ru>
2022-04-06 01:46:12 +0300

    realize validation by count of arguments

--- LaboratoryWork6/lab6/lab-client/src/main/java/lab/client/CommandFactory.java
+++ LaboratoryWork6/lab6/lab-client/src/main/java/lab/client/CommandFactory.java
@@ -30,45 +30,87 @@ public class CommandFactory {
         try {
             switch (name) {
                 case "add":
+                    if (args.size() != 3) {
+                        return null;
+                    }
                     dragon = listener.inputDragon(args.get(0), args.get(1), args.get(2));
                     if (dragon == null) {
                         throw new IllegalArgumentException("Аргументы команды некорректны");
                     }
                     return new AddCommand(dragon);
                 case "add_if_max":
+                    if (args.size() != 3) {
+                        return null;
+                    }
                     dragon = listener.inputDragon(args.get(0), args.get(1), args.get(2));
                     if (dragon == null) {
                         throw new IllegalArgumentException("Аргументы команды некорректны");
                     }
                     return new AddIfMaxCommand(dragon);
                 case "add_if_min":
+                    if (args.size() != 3) {
+                        return null;
+                    }
                     dragon = listener.inputDragon(args.get(0), args.get(1), args.get(2));
                     if (dragon == null) {
                         throw new IllegalArgumentException("Аргументы команды некорректны");
                     }
                     return new AddIfMinCommand(dragon);
                 case "clear":
+                    if (args.size() != 0) {
+                        return null;
+                    }
                     return new ClearCommand();
                 case "exit":
+                    if (args.size() != 0) {
+                        return null;
+                    }
                     return new ExitCommand();
                 case "help":
+                    if (args.size() != 0) {
+                        return null;
+                    }
                     return new HelpCommand();
                 case "history":
+                    if (args.size() != 0) {
+                        return null;
+                    }
                     return new HistoryCommand();
                 case "info":
+                    if (args.size() != 0) {
+                        return null;
+                    }
                     return new InfoCommand();
                 case "max_by_cave":
+                    if (args.size() != 0) {
+                        return null;
+                    }
                     return new MaxByCaveCommand();
                 case "print_ascending":
+                    if (args.size() != 0) {
+                        return null;
+                    }
                     return new PrintAscendingCommand();
                 case "print_descending":
+                    if (args.size() != 0) {
+                        return null;
+                    }
                     return new PrintDescendingCommand();
                 case "remove_by_id":
+                    if (args.size() != 1) {
+                        return null;
+                    }
                     long id = Long.parseLong(args.get(0));
                     return new RemoveByIdCommand(id);
                 case "show":
+                    if (args.size() != 0) {
+                        return null;
+                    }
                     return new ShowCommand();
                 case "update_by_id":
+                    if (args.size() != 1) {
+                        return null;
+                    }
                     long idOfDragon = Long.parseLong(args.get(0));
                     dragon = listener.inputDragonWithPrimitives();
                     return new UpdateByIdCommand(idOfDragon, dragon);
