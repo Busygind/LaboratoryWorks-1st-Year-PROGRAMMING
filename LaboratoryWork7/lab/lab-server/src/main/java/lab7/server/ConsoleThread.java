239b4f1d6162cdeae0fe6582d71aae785083e48b
Dmirtiy Busygin <Busygin.voll@mail.ru>
2022-05-01 01:17:04 +0300

    update ConsoleThread

--- LaboratoryWork7/lab/lab-server/src/main/java/lab7/server/ConsoleThread.java
+++ LaboratoryWork7/lab/lab-server/src/main/java/lab7/server/ConsoleThread.java
@@ -3,14 +3,14 @@ package lab7.server;
 import java.util.Scanner;
 
 public class ConsoleThread extends Thread {
-    private static final Scanner scanner = ServerConfig.SCANNER;
+    private static final Scanner SCANNER = new Scanner(System.in);
     private volatile boolean running = true;
 
     @Override
     public void run() {
         ServerConfig.LOGGER.info("Console thread is running");
         while (running) {
-            String line = scanner.nextLine();
+            String line = SCANNER.nextLine();
             if ("exit".equalsIgnoreCase(line)) {
                 ServerConfig.LOGGER.info("Server closed");
                 System.exit(0);
