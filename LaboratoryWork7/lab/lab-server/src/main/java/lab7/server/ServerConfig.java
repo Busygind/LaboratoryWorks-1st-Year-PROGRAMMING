1b02c5482c61baf24a5016b56c63f613cd2bb9df
Dmirtiy Busygin <Busygin.voll@mail.ru>
2022-05-01 01:15:39 +0300

    delete rudimentary statics

--- LaboratoryWork7/lab/lab-server/src/main/java/lab7/server/ServerConfig.java
+++ LaboratoryWork7/lab/lab-server/src/main/java/lab7/server/ServerConfig.java
@@ -3,11 +3,8 @@ package lab7.server;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.util.Scanner;
-
 public class ServerConfig {
     public static final CollectionManager MANAGER = new CollectionManager();
     public static final Logger LOGGER = LogManager.getLogger();
     protected static final int SERVER_PORT = 45846;
-    protected static final Scanner SCANNER = new Scanner(System.in);
 }
