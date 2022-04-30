837fd1f1a05f7524da20fdbd3768bfb1414c43dc
Dmirtiy Busygin <Busygin.voll@mail.ru>
2022-05-01 01:07:17 +0300

    refactor exit-system

--- LaboratoryWork7/lab/lab-server/src/main/java/lab7/server/exceptions/DisconnectInitException.java
+++
@@ -1,9 +0,0 @@
-package lab7.server.exceptions;
-
-import java.io.IOException;
-
-public class DisconnectInitException extends IOException {
-    public DisconnectInitException(String message) {
-        super(message);
-    }
-}
